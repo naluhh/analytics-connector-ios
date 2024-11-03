b415f58170d9fdf01eb48d52998235a0d9127c26
Charly <charly.delaroche@gmail.com>
2024-11-03 22:13:01 +0100

    add vision os support

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .iOS(.v10),
         .macOS(.v10_13),
         .tvOS(.v9),
-        .watchOS(.v3)
+        .watchOS(.v3),
+        .visionOS(.v1),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
